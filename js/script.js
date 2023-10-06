aa9ae0cd54cc212b88f3eba7b0c5f16973f2616a
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2023-10-06 09:46:07 +0200

    Add title list generation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-// document.getElementById('test-button').addEventListener('click', function(){
-//     const links = document.querySelectorAll('.titles a');
-//     console.log('links:', links);
-//   });
-
+{
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -39,9 +35,43 @@ const titleClickHandler = function(event){
     targetArticle.classList.add('active');
     console.log('targetArticle:', targetArticle);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    
+  /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for(let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    /* insert link into titleList */
+    html = html + linkHTML;
+    console.log(html);
+    }
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
   
-const links = document.querySelectorAll('.titles a');
-  
-for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+    for(let link of links){
+        link.addEventListener('click', titleClickHandler);
+    }
+}
+
+generateTitleLinks();
+
 }
